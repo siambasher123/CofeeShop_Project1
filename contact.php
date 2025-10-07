bd8d881bc1c50b049a25daf3e3648d9709154516
Your Name <your@example.com>
2025-10-07 23:54:53 +0600

    solving errors in the contact.php

--- contact.php
+++ contact.php
@@ -1,29 +1,23 @@
-<?php
-session_start();
-include_once 'config.php';
-
-// Handle form submission
-$success_msg = "";
-if(isset($_POST['submit'])){
-    $name = $conn->real_escape_string($_POST['name']);
-    $email = $conn->real_escape_string($_POST['email']);
-    $message = $conn->real_escape_string($_POST['message']);
-
-    $sql = "INSERT INTO contacts (name, email, message) VALUES ('$name', '$email', '$message')";
-    if($conn->query($sql)){
-        $success_msg = "Your message has been sent successfully!";
-    } else {
-        $success_msg = "Error sending message. Please try again.";
-    }
-}
-?>
-
-
-
-</div>
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark navbar-custom">
+    <div class="container">
+        <a class="navbar-brand" href="index.php">Coffee Bliss</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="menu.php">Menu</a></li>
+                <li class="nav-item"><a class="nav-link" href="about.php">About Us</a></li>
+                <li class="nav-item"><a class="nav-link active" href="contact.php">Contact</a></li>
+                <?php if(isset($_SESSION['user_id'])): ?>
+                    <li class="nav-item"><a class="nav-link" href="cart.php">Cart</a></li>
+                    <li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
+                <?php else: ?>
+                    <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
+                <?php endif; ?>
+            </ul>
+        </div>
     </div>
-</div>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+</nav>
